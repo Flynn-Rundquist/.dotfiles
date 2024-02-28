8d9fb13dda33d71f4dc9769fea90185b0273b8b1
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-02-28 09:55:34 -0500

    getting lazy

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,9 +2,9 @@ return {
     "nvim-lualine/lualine.nvim",
     config = function()
         require("lualine").setup({
-        options = {
-            theme = "gruvbox_dark"
-        },
+            options = {
+                theme = "gruvbox_dark"
+            },
         })  
     end
-    }  
+}
