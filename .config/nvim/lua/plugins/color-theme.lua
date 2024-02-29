2400d96a011d6d8fdeb6ba40fed6ab3d40194f4d
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-02-29 09:29:00 -0500

    color theme

--- .config/nvim/lua/plugins/color-theme.lua
+++ .config/nvim/lua/plugins/color-theme.lua
@@ -1,7 +1,7 @@
 -- luachech: ignore vim
 return {
-    "nyoom-engineering/nyoom.nvim"
-    lazy = false 
+    "nyoom-engineering/nyoom.nvim",
+    lazy = false,
     priority = 1000,
     config = function()
         require("github-theme").setup({})
